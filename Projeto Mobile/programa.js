f48ebe31dc370a949b87ca367909e6ce94812524
Magno Veras <magno.veras88@gmail.com>
2018-04-18 12:23:48 -0300

    Página de descrição implementada

--- Projeto Mobile/programa.js
+++ Projeto Mobile/programa.js
@@ -1,5 +1,6 @@
 var app = angular.module('loja', []);
 
+
 app.controller('principal', function($scope, $http){
 
 
@@ -16,9 +17,43 @@ app.controller('principal', function($scope, $http){
       return produto.valor >= $scope.min && produto.valor <= $scope.max;
   };
 
+  //retorna valor de categoria para a tela
   $scope.categoria = $('#categorias').val();
+
+  //Função Captura produto clicado
+  $scope.selecionar = function(nometela){                   
+    var i = 0;
+    var a = false;
+    while (i < $scope.produtos.length && a === false){
+        if ($scope.produtos[i].nome === nometela){
+            a = true;
+        } else {
+            i++;
+        }
+    }
+    
+    window.sessionStorage.setItem('produto', JSON.stringify($scope.produtos[i]));
+    //alert("Passou!!!");
+    $scope.teste = {};
+    $scope.produtodesc = JSON.parse(sessionStorage.getItem('produto'));    
+    window.location.href='descricao.html';
+    }
+
+    
 });
 
+//Controller da página de descrição
+var appdesc = angular.module("produtodesc", []);
+
+appdesc.controller('descricao', function($scope, $http){
+    $scope.produtodesc = {};
+               $scope.init = function(){                   
+                   $scope.produtodesc = JSON.parse(window.sessionStorage.getItem('produto'));
+                   //alert("Página descrição" + $scope.produtodesc.nome);
+                   window.sessionStorage.removeItem('produto');
+               }
+    $scope.init();
+});
 
 $(document).ready(function(){
     $('#btnaplicar').click(function(){
